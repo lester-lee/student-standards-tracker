26dbd6388c99d015bbed94b7192bae6dd3b09600
Lester Lee <contact@lester-lee.com>
2021-11-21 15:11:19 -0500

    Style standard

--- main.js
+++ main.js
@@ -14,18 +14,32 @@ let state = {
 // Setup
 //-----------------------------
 // Canvas options
-let canvas;
+let canvas = d3.select("#StandardsViz");
 const WIDTH = 800;
 const HEIGHT = 600;
 
+const masteryColorScale = d3
+  .scaleOrdinal()
+  .domain(["0", "1", "2", "3"])
+  .range(["#de425b", "#dbc667", "#79ab62", "#488f31"]);
+
+// Set up legend
+d3.select(".LegendKeys")
+  .selectAll(".LegendKeys .Standard")
+  .data([0, 1, 2, 3])
+  .join("li")
+  .attr("class", "Standard")
+  .style("background-color", (d) => masteryColorScale(d))
+  .append("span")
+  .attr("class", "StandardLabel")
+  .text(d => d);
+
 // Load in course metadata
-const CourseSelector = document.querySelector("#CourseSelector");
-const StudentSelector = document.querySelector("#StudentSelector");
+const courseSelector = document.querySelector("#CourseSelector");
+const studentSelector = document.querySelector("#StudentSelector");
 d3.json("data/courses.json").then((courses) => {
   state.courses = courses;
   updateCourseDropdown();
-  updateStudentDropdown();
-  init();
 });
 
 //-----------------------------
@@ -39,14 +53,15 @@ const createOption = (name, value) => {
   return option;
 };
 
-/** Creates an option for each course */
+/** Creates an option for each course and update students */
 const updateCourseDropdown = () => {
   state.courses.forEach((course, idx) => {
-    CourseSelector.appendChild(createOption(course.title, idx));
+    courseSelector.appendChild(createOption(course.title, idx));
   });
+  updateStudentDropdown();
 };
 
-/** Creates an option for each student in current course */
+/** Creates an option for each student in current course and load standards */
 const updateStudentDropdown = () => {
   const students = state.courses[state.currentCourseIdx].roster.map((student) =>
     createOption(
@@ -54,54 +69,46 @@ const updateStudentDropdown = () => {
       `${student.first}_${student.last}`
     )
   );
-  StudentSelector.replaceChildren(...students);
+  studentSelector.replaceChildren(...students);
+  const student = studentSelector.firstChild.value;
+  loadStandards(student);
+};
+
+/** Updates current standards in state to match student */
+const loadStandards = (student) => {
+  // Get path of masteries for selected student
+  const currentCourseName = state.courses[state.currentCourseIdx].name;
+  const studentPath = `data/courses/${currentCourseName}/students/${student}.csv`;
+
+  d3.csv(studentPath, d3.autoType).then((standards) => {
+    state.currentStandards = standards;
+    render();
+  });
 };
 
 /** Change current course and update students / standards. */
-CourseSelector.addEventListener("change", (event) => {
+courseSelector.addEventListener("change", (event) => {
   state.currentCourseIdx = parseInt(event.target.value);
   updateStudentDropdown();
 });
 
 /** Load in data for the selected student and rerender */
-StudentSelector.addEventListener("change", (event) => {
-  // Get path of masteries for selected student
-  const currentCourseName = state.courses[state.currentCourseIdx].name;
-  const studentPath = `data/courses/${currentCourseName}/students/${event.target.value}.csv`;
-
-  d3.csv(studentPath, d3.autoType).then((standards) => {
-    state.currentStandards = standards;
-    console.log("state.currentStandards :>> ", state.currentStandards);
-    render();
-  });
+studentSelector.addEventListener("change", (event) => {
+  loadStandards(event.target.value);
 });
 
 //-----------------------------
 // Render
 //-----------------------------
-
-/** This function runs after courses are loaded */
-const init = () => {
-  canvas = d3.select(".StandardsViz");
-  render();
-};
-
 /** Called whenever there is an update to data/state */
 const render = () => {
-  const masteryColorScale = d3
-    .scaleOrdinal()
-    .domain(["0", "1", "2", "3"])
-    .range(["red", "purple", "blue", "green"]);
-
   let standards = canvas
     .selectAll(".Standard")
     .data(state.currentStandards)
     .join("div")
     .attr("class", "Standard")
-    .attr("style", (d, i) => {
-      return `background-color:${masteryColorScale(
-        d.mastery
-      )};border:5px solid black;`;
-    })
-    .text((d) => d.code);
+    .style("background-color", (d) => masteryColorScale(d.mastery))
+    .append("span")
+    .attr("class", "StandardLabel")
+    .text(d => d.code);
 };
